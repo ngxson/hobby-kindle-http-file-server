04f6d47c4e2e96a428cd1dcd8dbf100e961e79ce
ngxson <thichthat@gmail.com>
2020-09-17 00:39:41 +0200

    init

---
+++ server.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+(/mnt/us/extensions/filebrowser/filebrowser -a 0.0.0.0 -p 80 -r "/mnt/us") &
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Press Ctrl-C to stop"
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 99999999
---
+++ start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/mnt/us/extensions/kterm/bin/kterm -e "bash /mnt/us/extensions/filebrowser/server.sh" -k 1 -o U -s 7
